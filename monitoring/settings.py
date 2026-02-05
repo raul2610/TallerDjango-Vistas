7590646f04072c73de67e97614800caa3f25de08
Raúl <rs.ruiza1@uniandes.edu.co>
2026-02-04 19:16:50 -0500

    config -> update database settings for development

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "tallersemana3",
+        "USER": "raul",
+        "PASSWORD": "182603",
         "HOST": "localhost",
         "PORT": "",
     }
